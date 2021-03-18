2f88859741c4c5132945fa1aa3e1bb12654d9925
Danilo de Arruda Kuhlmann <danilo.kuhlmann@hotmail.com>
2021-03-18 08:32:57 -0300

    Adicionar arquivos de projeto.

---
+++ AI/Classes/AI.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AI.Classes
+{
+    public class AI
+    {
+        public void Training(List<Object> Data)
+        {
+            var context = new MLContext();
+        }
+        public void TestModel(List<Object> model)
+        {
+
+        }
+        public void Load(Object model)
+        {
+
+        }
+        public void Predict(Object Data)
+        {
+
+        }
+    }
+}
