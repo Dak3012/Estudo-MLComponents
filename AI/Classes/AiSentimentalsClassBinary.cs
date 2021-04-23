d017caf6b928724d5b1aa78d13d7d994b059d237
Danilo de Arruda Kuhlmann <danilo.kuhlmann@hotmail.com>
2021-04-22 22:37:13 -0300

    Finalizalção da analise de sentimento de forma binaria

---
+++ AI/Classes/AiSentimentalsClassBinary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML.Transforms.Text;
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+namespace AI.Classes
+{
+    public class AiSentimentalsClassBinary
+    {
+        public PredictionEngine<SentimentData, SentimentPrediction> Prediction { get; private set; }
+        public CalibratedBinaryClassificationMetrics metricas { get; private set; }
+        private string DataPath;
+        private MLContext Context;
+        private ITransformer model;
+
+        public AiSentimentalsClassBinary(string Path)
+        {
+            DataPath = Path;
+            Context = new MLContext();
+            var arquivo = LoadData();
+            model = BuildAndTrainModel(arquivo.TrainSet);
+            metricas = Evaluate(arquivo.TestSet);
+            
+        }
+        public AiSentimentalsClassBinary(ITransformer model)
+        {
+            model = this.model;
+            Context = new MLContext();
+            Prediction = Context.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+        }
+        public TrainTestData LoadData()
+        {
+            var DataView = Context.Data.LoadFromTextFile<SentimentData>(DataPath, hasHeader: false);
+            var SplitDataView = Context.Data.TrainTestSplit(DataView, testFraction: 0.2);            
+            return SplitDataView;
+        }
+        public ITransformer BuildAndTrainModel(IDataView SplitTrainTest)
+        {
+            var textFeaturizing = Context.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText));
+            var sdcalogistic = Context.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features");
+            var estimator = textFeaturizing.Append(sdcalogistic);
+            var model = estimator.Fit(SplitTrainTest);
+            Prediction = Context.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            return model;
+        }
+        public CalibratedBinaryClassificationMetrics Evaluate(IDataView splitTest)
+        {
+            var prediction = model.Transform(splitTest);
+            var metrics = Context.BinaryClassification.Evaluate(prediction, "Label");
+            return metrics;
+        }
+        public PredictionEngine<SentimentData, SentimentPrediction> CarregarModelo(ITransformer model) //Utilizar para carregar um modelo
+        {
+            var predictionFunction = Context.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            return predictionFunction;
+        }
+        public IEnumerable<SentimentPrediction> UseModelWithBatchItems(IEnumerable<SentimentData> sentiments)
+        {
+            var bathcomments = Context.Data.LoadFromEnumerable(sentiments);
+            var preditictions = model.Transform(bathcomments);
+            return Context.Data.CreateEnumerable<SentimentPrediction>(preditictions, reuseRowObject: false);
+        }
+
+    }
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText;
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment;
+    }
+    public class SentimentPrediction : SentimentData
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
---
+++ AITests/Classes/AiSentimentalsClassTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AI.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AI.Classes.Tests
+{
+    [TestClass()]
+    public class AiSentimentalsClassTests
+    {
+        [TestMethod()]
+        public void BuildAndTrainModelTest()
+        {
+            var DirStringEnvironment = Environment.GetEnvironmentVariable("Location");
+            var path = Path.Combine(DirStringEnvironment, "yelp_labelled.txt");
+            var createContext = new AiSentimentalsClassBinary(path);
+            SentimentData sampleStatement = new SentimentData
+            {
+                SentimentText = "This was a very bad steak"
+            };
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData
+                {
+                    SentimentText = "This was a horrible meal"
+                },
+                new SentimentData
+                {
+                    SentimentText = "I love this spaghetti."
+                }
+            };
+            var predict = createContext.Prediction.Predict(sampleStatement);
+            var lists2 = sentiments.Select(x => createContext.Prediction.Predict(x));
+            var lists = createContext.UseModelWithBatchItems(sentiments).ToList(); //modelo apresentou lentidão em relação ao predict com lambda.
+            
+        }
+    }
+}
